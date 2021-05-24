9a1c8310d954cd4495abf2a53d41e9185855139e
pkadakia23@gmail.com <pkadakia23@gmail.com>
2021-05-24 10:05:38 -0400

    final push

--- tax.php
+++ tax.php
@@ -5,8 +5,10 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
+// Get the input from JavaScript
 $jsonData = json_encode(file_get_contents('php://input'));
 
+// Check if the state is NY, create a JSON with taxTotal as 5.
 if($jsonData['shippingAddress']['state'] == 'NY'){
     $arr = array ('taxTotal'=>500);
 }else{
